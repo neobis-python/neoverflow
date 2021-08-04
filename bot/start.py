026b1c7e7219b1011472876640e26850ae9bf5b1
Dimash <igissinov_d@auca.kg>
2021-08-04 17:34:28 +0600

    feat: install packages, add start script

---
+++ bot/start.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = os.getenv("API_TOKEN")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start", "help"])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
